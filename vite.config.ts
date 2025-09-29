f063409a006591fa190a08bd27e731b331a4f79a
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-09-29 14:10:09 +0700

    fix: configure path alias to resolve '@/src'

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import generouted from '@generouted/react-router/plugin';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), generouted()],
   resolve: {
     alias: {
-      '@': './src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
