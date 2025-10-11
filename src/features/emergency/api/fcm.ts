3864d6be8dac3f52e0a256bfa89c448a7ad17988
zersix225 <drago.overlond@gmail.com>
2025-10-12 05:57:43 +0700

    feat: setup api call from backend

---
+++ src/features/emergency/api/fcm.ts
@@ -0,0 +1,11 @@
+import { post } from '.';
+import type { SuccessResponseInterface } from '../interfaces/api';
+import type { NotificationT, TokenModel } from '../interfaces/common';
+
+export default class FCMApi {
+  static async sendAllNotification(
+    notification: NotificationT
+  ): Promise<SuccessResponseInterface<TokenModel>> {
+    return await post('/notifications/all', { notification });
+  }
+}
---
+++ src/features/emergency/api/token.ts
@@ -0,0 +1,18 @@
+import { post } from '.';
+import type { SuccessResponseInterface } from '../interfaces/api';
+import type { TokenModel } from '../interfaces/common';
+
+export default class TokenApi {
+  // static async getTokens() {
+  //     const response: SuccessResponseInterface<TokenModel> = await get('/tokens');
+  //     return response;
+  // }
+
+  static async storeToken(token: string) {
+    const response: SuccessResponseInterface<TokenModel> = await post(
+      '/tokens',
+      { tokens: token }
+    );
+    return response;
+  }
+}
