9b1b58fb9f49d740009887d21ae1ff7c674e4d18
kykkrm <nonlada.prem@mail.kmutt.ac.th>
2025-10-25 00:12:23 +0700

    create demo of detect harm page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import DetectHarmPage from './features/G16-CommunitySupportMap/pages/detectHarmpage';
+
+const router = createBrowserRouter([
+  {
+    path: '/harm',
+    element: <DetectHarmPage />,
+  },
+]);
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline"> Hello fellow CS-ers! </h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import { Routes } from '@generouted/react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Routes />
-    </QueryClientProvider>
+    <App />
   </StrictMode>
 );
