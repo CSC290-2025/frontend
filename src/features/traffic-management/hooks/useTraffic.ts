df4262b74d0f904fcb417f6728b5c7fb6b18c354
SittaWetpa <sitta.wetpa@mail.kmutt.ac.th>
2025-11-01 10:30:05 +0700

    Create Layout For Frontend

---
+++ src/features/traffic-management/hooks/useTraffic.ts
@@ -0,0 +1,16 @@
+//import
+
+function useTrafficLights() {}
+
+function useTrafficLight(id: string) {}
+
+function useTrafficDensity() {}
+
+function useUpdateTrafficLight() {}
+
+export default {
+  useTrafficLights,
+  useTrafficLight,
+  useTrafficDensity,
+  useUpdateTrafficLight,
+};
