c664ceb28c6c169c09c955a7e9abc919914e86ba
BBelleysp <yossapan.r@gmail.com>
2025-11-02 22:07:47 +0700

    chore: Update banners array with new content

--- src/features/freecycle/pages/Constants.ts
+++ src/features/freecycle/pages/Constants.ts
@@ -49,3 +49,33 @@ export interface RequestItem {
   updated_at: string;
   post_item?: PostItem;
 }
+
+export const banners = [
+  {
+    gradient: 'from-red-100 to-pink-300',
+    title: 'Share & Care',
+    subtitle: 'Give items a second life',
+  },
+  {
+    gradient: 'from-purple-100 to-indigo-300',
+    title: 'Connect & Share',
+    subtitle: 'Come to our Giveaway event – see items and take what you like!',
+  },
+  {
+    gradient: 'from-white-100 to-purple-300',
+    title: 'Join Our Team',
+    subtitle:
+      'We are looking for experts from various fields to join our event!',
+  },
+  {
+    gradient: 'from-teal-100 to-cyan-300',
+    title: 'Recycle & Reuse',
+    subtitle: 'Build a sustainable community',
+  },
+  {
+    gradient: 'from-green-100 to-yellow-300',
+    title: 'Ready for Rehoming',
+    subtitle:
+      'Plenty of great items are waiting for you to discover and take home.',
+  },
+];
