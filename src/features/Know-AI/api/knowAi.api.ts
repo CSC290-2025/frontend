544b318ce63f3ce54645c9b7552c95cdf315554f
natebps <natebpss@gmail.com>
2025-11-04 16:24:47 +0700

    feat: course feature but only type

---
+++ src/features/Know-AI/api/knowAi.api.ts
@@ -0,0 +1,13 @@
+// import { apiClient } from "@/lib/apiClient";
+// import { SuccessResponseInterface } from "../interfaces/api";
+
+// export async function fetchAllCourses(params?: {
+//   search?: string;
+//   type?: CourseType;
+//   status?: CourseStatus;
+//   page?: number;
+//   limit?: number;
+// }) {
+//   const res = await apiClient.get<Course[]>("/courses", { params });
+//   return res.data;
+// }
