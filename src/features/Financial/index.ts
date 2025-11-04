a52c545b426f404e5cc1fab7f4baf0fc9fc75f66
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-11-04 07:15:45 +0700

    feat: add page to index and some documentations for financial developers

---
+++ src/features/Financial/index.ts
@@ -0,0 +1,6 @@
+// re-exporting parts that other features might use
+export * from './hooks';
+export * from './api';
+export * from './types';
+export { default as FinancialPage } from './pages/FinancialPage';
+export { useUserWallets } from './hooks/useUserWallets';
