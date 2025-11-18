d6fdc5473c7cd3368662b5489e4cde950dd8e7c7
Nicha-h <nichahongsri@gmail.com>
2025-11-19 05:01:41 +0700

    feat: added finance api

---
+++ orval.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'orval';
+
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3000';
+
+export default defineConfig({
+  api: {
+    input: {
+      // if putting frontend & backend repos in the same root folder
+      // otherwise, use './{name}.json' maybe and put it in gitignore
+      target: '../openapi.json',
+      filters: {
+        mode: 'include',
+        tags: ['Wallets', 'MetroCards', 'Insurance Cards', 'SCB'],
+      },
+      // target: `${BACKEND_URL}/doc`,
+    },
+    output: {
+      mode: 'tags',
+      target: './src/api/generated',
+      schemas: './src/api/generated/model',
+      client: 'react-query',
+      httpClient: 'axios',
+      mock: false, // can be changed to true & use this guide [https://orval.dev/reference/configuration/output#mock]
+      clean: true,
+      prettier: true,
+      baseUrl: `${BACKEND_URL}`,
+    },
+  },
+});
