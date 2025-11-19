abb7f300f73b3374e399adda1ae86e78f612c68b
SNOWxSAUSAGES <152620609+SNOWxSAUSAGES@users.noreply.github.com>
2025-11-19 17:13:27 +0700

    ReMastered Admin page ee

--- src/features/traffic-management/components/TrafficLightsList.tsx
+++ src/features/traffic-management/components/TrafficLightsList.tsx
@@ -166,7 +166,7 @@ export default function TrafficLightsList({
   };
 
   return (
-    <Card className="ml-4 h-[calc(100vh-8rem)] w-80 flex-shrink-0">
+    <Card className="ml-4 h-full w-80 flex-shrink-0">
       <CardHeader className="pb-3">
         <CardTitle className="text-lg">Traffic Lights</CardTitle>
         <div className="relative mt-2">
