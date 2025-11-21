a925a9d67850a235188627ccb710b4233d090f0c
quazer02 <wasin.phak@mail.kmutt.ac.th>
2025-11-21 22:50:30 +0700

    feat: router.ts

--- src/router.ts
+++ src/router.ts
@@ -5,50 +5,47 @@ import { components, hooks, utils } from '@generouted/react-router/client';
 
 export type Path =
   | `/`
-  | `/clean-air/district-detail/:district`
-  | `/clean-air/district-selection`
-  | `/clean-air/overview/:district`
-  | `/example`
-  | `/example/:id`
-  | `/weather-aqi`
-  | `/weather-aqi/overview/:district`;
   | `/Know-AI/:course`
   | `/Know-AI/:course/:id`
   | `/Know-AI/createCourse`
   | `/Know-AI/exercises`
   | `/Know-AI/exercises/:level/:question`
-  | `/example`
-  | `/example/:id`
-  | `/weather`
-  | `/weatherCity`
-  | `/weatherMain`;
-  | `/dashboard`
-  | `/district-detail/:district`
-  | `/district-selection`
+  | `/activity`
+  | `/chat`
+  | `/clean-air/district-detail/:district`
+  | `/clean-air/district-selection`
+  | `/demo-tracking`
   | `/example`
   | `/example/:id`
   | `/harm`
   | `/harm/:id`
   | `/healthcare`
+  | `/hotLine`
+  | `/login`
   | `/map`
   | `/map/:id`
-  | `/login`
   | `/power-bi`
   | `/power-bi/:type/:category`
   | `/power-bi/:type/:category/:id`
   | `/power-bi/create`
   | `/power-bi/edit/:id`
-  | `/register`;
+  | `/register`
+  | `/sos`
+  | `/sos/:id`
+  | `/sos/report`
+  | `/sos/report/:id`
+  | `/weather`
+  | `/weather-aqi`
+  | `/weather-aqi/overview/:district`
+  | `/weatherCity`
+  | `/weatherMain`;
 
 export type Params = {
-  '/clean-air/district-detail/:district': { district: string };
-  '/clean-air/overview/:district': { district: string };
   '/Know-AI/:course': { course: string };
   '/Know-AI/:course/:id': { course: string; id: string };
   '/Know-AI/exercises/:level/:question': { level: string; question: string };
-  '/district-detail/:district': { district: string };
+  '/clean-air/district-detail/:district': { district: string };
   '/example/:id': { id: string };
-  '/weather-aqi/overview/:district': { district: string };
   '/harm/:id': { id: string };
   '/map/:id': { id: string };
   '/power-bi/:type/:category': { type: string; category: string };
@@ -58,6 +55,9 @@ export type Params = {
     id: string;
   };
   '/power-bi/edit/:id': { id: string };
+  '/sos/:id': { id: string };
+  '/sos/report/:id': { id: string };
+  '/weather-aqi/overview/:district': { district: string };
 };
 
 export type ModalPath = never;
