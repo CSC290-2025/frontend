a4c169f1f9a14fad46f454c5a980507a363160b1
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-11-21 20:11:58 +0700

    feat(volunteer): export UserPage component to enable its usage in the app

--- src/features/volunteer/index.ts
+++ src/features/volunteer/index.ts
@@ -1,3 +1,3 @@
 export { default as UserCard } from './components/UserCard';
-// export { default as UserPage } from './pages/UserPage';
+export { default as UserPage } from './pages/UserPage';
 export { useUser } from './hooks/useUser';
