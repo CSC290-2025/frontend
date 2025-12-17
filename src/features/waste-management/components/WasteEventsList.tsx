34b3710113c293b7674c8bace6a8a0510c2e3f3b
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-12-17 23:07:00 +0700

    fix: fix of waste event page

--- src/features/waste-management/components/WasteEventsList.tsx
+++ src/features/waste-management/components/WasteEventsList.tsx
@@ -62,7 +62,7 @@ export default function WasteEventsList() {
 
   return (
     <>
-      <div className="space-y-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {events.map((event: any) => (
           <div
             key={event.id}
